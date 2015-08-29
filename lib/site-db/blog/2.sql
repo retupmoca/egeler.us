2fc55e37e86a271b7f5593851ff5272a26954dfd
Andrew Egeler <andrew@egeler.us>
2015-08-29 16:59:12 -0400

    split tags to extra table

--- lib/site-db/blog/2.sql
+++ lib/site-db/blog/2.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `posts` DROP COLUMN `tags`;
 
-CREATE TABLE `post_tags` {
+CREATE TABLE `post_tags` (
   `post_id` int(11) NOT NULL,
   `tag` varchar(50) NOT NULL
-} ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
