2408d2ee33be53b24fa2b4f7d4c6c56914afbff7
Andrew Egeler <andrew@egeler.us>
2015-10-31 20:11:58 -0400

    ...but Crust.request uses different name than PSGI

--- lib/Site.pm
+++ lib/Site.pm
@@ -50,7 +50,7 @@ method handle(%env) {
     if %env<HTTP_HOST> eq 'retupmoca.com' {
         return Page::Redirect.go(:code(301), :url('https://egeler.us/blog/u/andrew'));
     }
-    elsif %env<HTTP_HOST> ne 'egeler.us' || !$request.secure {
+    elsif %env<HTTP_HOST> ne 'egeler.us' || !%env<HTTPS> {
         return Page::Redirect.go(:code(301), :url('https://egeler.us/'));
     }
     else {
